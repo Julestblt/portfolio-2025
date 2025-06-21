dfb91067c55e05918cd0c868ae60d004a984ed33
JuleZ <julestblt.dev@pm.me>
2025-06-21 14:39:35 +0200

    feat: update title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,11 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>Jules THIBAULT - Portfolio</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
 <ModeWatcher defaultMode="system" />
 <ThemeSparkles />
 {@render children()}
